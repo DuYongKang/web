e2ceeec1047df0729303e45f240c649cca96a2b3
DuKang <1577601569@qq.com>
2017-09-22 00:12:46 +0800

    状态不对

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './test_logo.png';
 function component() {
     var element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
   
+    //添加图片
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
     return element;
   }
   
